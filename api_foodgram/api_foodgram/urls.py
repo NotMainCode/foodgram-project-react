7464c6da3fa30783c2b6feee748a548dcaf57e7e
NotMainCode <106552134+NotMainCode@users.noreply.github.com>
2023-02-22 01:24:06 +0300

    API documentation is available at the redoc/ endpoint in debug mode

--- api_foodgram/api_foodgram/urls.py
+++ api_foodgram/api_foodgram/urls.py
@@ -9,16 +9,17 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("api/", include("api.urls")),
     path("admin/", admin.site.urls),
-    path(
-        "redoc/",
-        TemplateView.as_view(template_name="redoc.html"),
-        name="redoc",
-    ),
 ]
+
 if settings.DEBUG:
     urlpatterns += (
         path("__debug__/", include("debug_toolbar.urls")),
         path("api-auth/", include("rest_framework.urls")),
+        path(
+            "redoc/",
+            TemplateView.as_view(template_name="redoc.html"),
+            name="redoc",
+        ),
     )
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
